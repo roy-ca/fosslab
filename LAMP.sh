fb5b952b34d1d9a903024a21def0d87fcd0f92c1
roy-ca <royca98@gmail.com>
2018-04-10 15:20:03 +0530

    Add files via upload

---
+++ LAMP.sh
@@ -0,0 +1,14 @@
+//To install Apache
+sudo apt-get install apache2
+
+//To Check Apache
+Open a web browser and navigate to http://localhost/. You should see a message saying It works!
+
+//To install MySQL
+sudo apt-get install mysql-server
+
+//To install PHP
+sudo apt-get install php7.1-cli
+
+//To Check PHP
+php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
